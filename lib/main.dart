d189b1628ddad3e88c401d5868be562345a8e531
CodingThanos <64342840+jeongwoo0427@users.noreply.github.com>
2023-10-17 23:31:06 +0900

    feat : 타일맵 이미지와 타일맵 설정들을 적용하여 맵을 생성시킴

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterflametest/pixel_adventure.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  Flame.device.fullScreen(); //풀 화면으로 생성
+  Flame.device.fullScreen(); //풀 화면 로드 (상단바 사라짐)
+  Flame.device.setLandscape(); //가로모드 고정
+
 
   PixelAdventure game = PixelAdventure();
-  runApp(GameWidget(game: game));
+  runApp(GameWidget(game: kDebugMode?PixelAdventure():game));
 }
