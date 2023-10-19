595c17ebb17ff00df474294f0bec11ed3cccb37b
CodingThanos <64342840+jeongwoo0427@users.noreply.github.com>
2023-10-19 21:16:17 +0900

    feat : 타일맵 로드 완료, 카메라 종횡비 적용

--- lib/levels/level.dart
+++ lib/levels/level.dart
@@ -1,10 +1,16 @@
 import 'package:flame/components.dart';
+import 'package:flame_tiled/flame_tiled.dart';
 class Level extends World{
 
+  late TiledComponent level;
 
   @override
   Future<void> onLoad() async{
+    
+    level = await TiledComponent.load('level-01.tmx', Vector2.all(16)); //해당 타일을 가로,세로 16 픽셀단위로 나눈후 가져온다.
 
+    add(level); //해당 타일맵을 컴포넌트로써 월드클래스에 추가한다.
+    
     return super.onLoad();
   }
 }
--- lib/pixel_adventure.dart
+++ lib/pixel_adventure.dart
@@ -1,5 +1,25 @@
+import 'package:flame/camera.dart';
 import 'package:flame/game.dart';
+import 'package:flame/widgets.dart';
+import 'package:flutterflametest/levels/level.dart';
 
 class PixelAdventure extends FlameGame{
 
+  late final CameraComponent cam;
+
+  final Level myWorld = Level();
+
+  @override
+  Future<void> onLoad() async{
+
+    cam = CameraComponent.withFixedResolution(width: 640, height: 360,world: myWorld ); //해당 월드의 종횡비와 맞게 적용해야 한다.
+    cam.viewfinder.anchor = Anchor.topLeft; //카메라 고정을 왼쪽 위로 하도록 함.
+
+
+    add(cam);
+    add(myWorld);
+    //또는 addAll([cam,myWorld]); 도 가능
+
+    return super.onLoad();
+  }
 }
